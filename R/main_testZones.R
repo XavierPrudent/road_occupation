9749c623c194d4a259b528e5b60407fb16264acd
XavierPrudent <prudentxavier@gmail.com>
2018-04-09 18:19:49 -0400

    range the facdep

--- R/main_testZones.R
+++ R/main_testZones.R
@@ -7,13 +7,12 @@
 ## Load general packages
 source("include_spatialFunctions.R")
 source("/Users/lavieestuntoucan/Civilia/tech/general/load_R_pkg.R")
+
 ##
 ## Input data: output of osrm
-d <- "out/allTrips.RDS"
+d <- "out/allTrips_1000_facdep.RDS"
 d <- readRDS(d)
-##
-## Count 1 for each path
-d@data$n <- 1
+
 ##
 ## Aggregate the paths
 d.ag <- aggit(d,"facdep")
@@ -24,13 +23,20 @@ map.city <- leaflet() %>%
   addTiles('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png') %>%
   setView(coord.TroisRivieres$lon, coord.TroisRivieres$lat, zoom = 11)
 
-
 ## color depends on flux
 map1 <- NULL
-for( i in 1:length(d.ag@lines)){
+df <- data.frame(i = seq_along(d.ag@lines), n = NA)
+
+for( i in seq_along(d.ag@lines)){
+  df$n[i] <- d.ag@data$facdep[i]
+}
+df <- df %>% arrange(n)
+
+for( i in df$i){
   i.x <- d.ag@lines[[i]]@Lines[[1]]@coords[,1]
   i.y <- d.ag@lines[[i]]@Lines[[1]]@coords[,2]
   i.n <- d.ag@data$facdep[i]
+  if( i.n == 0 ) next
   i.col <- col.n(i.n)
   if( is.null(map1) ) MAP <- map.city
   else MAP <- map1
